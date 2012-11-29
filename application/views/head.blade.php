fdcee058adb5475d24eb33ff427da5d68743a213
Martin Svec <martin@svec.eu>
2012-11-29 10:10:24 +0100

    Uprava

--- application/views/head.blade.php
+++ application/views/head.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Laravel: A Framework For Web Artisans</title>
+	<title>Vydavkovač</title>
 	<meta name="viewport" content="width=device-width">
 	{{ HTML::style('assets/css/bootstrap.min.css') }}
 	{{ HTML::style('assets/css/app.css') }}
@@ -31,7 +31,7 @@
 					<ul class="nav">
 					    <li<?php echo $active=='admin' ? ' class="active"' : ''; ?>>{{ HTML::link('admin', 'Administrácia'); }}</li>
 						<li<?php echo $active=='prijmy' ? ' class="active"' : ''; ?>>{{ HTML::link('/incomes', 'Príjmy'); }}</li>
-						<li<?php echo $active=='vydavky' ? ' class="active"' : ''; ?>>{{ HTML::link('/spendings', 'Výdavky'); }}</li>
+						<li<?php echo $active=='vydavky' ? ' class="active"' : ''; ?>>{{ HTML::link('spendings', 'Výdavky'); }}</li>
 						<li<?php echo $active=='sporenie' ? ' class="active"' : ''; ?>>{{ HTML::link('/savings', 'Sporenie'); }}</li>
 						<li<?php echo $active=='nastavenia' ? ' class="active"' : ''; ?>>{{ HTML::link('/settings', 'Nastavenia'); }}</li>
 				    </ul>
