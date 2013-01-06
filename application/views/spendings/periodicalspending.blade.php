8b50d5b4978b166d7c436b64a9e2671195d8c8cc
Scarabeon <michal.bielik@gmail.com>
2013-01-06 17:35:07 +0100

    Update application/views/spendings/periodicalspending.blade.php

--- application/views/spendings/periodicalspending.blade.php
+++ application/views/spendings/periodicalspending.blade.php
@@ -1,34 +1,48 @@
 @include('head')
-
+<script>var js_polozky = {{ $dzejson }}</script>
+<h3 style="color: #bc8f8f;">{{ $message }}</h3>
 <h2>V&yacute;davky</h2>
 @include('spendings/sp-submenu')
-<h4>Pravideln&yacute; v&yacute;davok</h4>
-<div class="input-prepend">
-     <span class="add-on">D&aacute;tum:</span>
-     <input class="span3" type="date">
+
+<h2>Pravideln&yacute; v&yacute;davok</h2>
+{{ Form::open('spendings/savespending?update=1', 'POST', array('class' => 'side-by-side')); }}
+<input type="hidden" name="hlavicka-id" id="hidden" value="{{ $vydavky[0]->id }}"/>
+
+<div class="thumbnail">
+    <h4>Syst&eacute;mov&eacute; spr&aacute;vy:</h4>
+     <div class="input-prepend" style="float:left;width:295px">
+          <span class="add-on">D&aacute;tum:</span>
+          <input name="datum" class="span3" type="text" placeholder="Deň.Mesiac.Rok" value="{{ date('d.m.Y', strtotime($vydavky[0]->d_datum)) }}">
+     </div>
+
+<div class="input-prepend" style="float:left;width:350px">
+     <span class="add-on">N&aacute;zov v&yacute;davku: </span>
+     <select class="span3">
+          <option>&nbsp;</option>
+          <option>&Scaron;abl&oacute;na 1</option>
+          <option>&Scaron;abl&oacute;na 2</option>
+          <option>&Scaron;abl&oacute;na 3</option>
+          <option>&Scaron;abl&oacute;na 4</option>
+          <option>&Scaron;abl&oacute;na 5</option>
+     </select>
 </div>
-<div class="input-prepend">
-        <span class="add-on">N&aacute;zov v&yacute;davku: </span>
-    <select class="span3">
-        <option>&nbsp;</option>
-        <option>&Scaron;abl&oacute;na 1</option>
-        <option>&Scaron;abl&oacute;na 2</option>
-        <option>&Scaron;abl&oacute;na 3</option>
-        <option>&Scaron;abl&oacute;na 4</option>
-        <option>&Scaron;abl&oacute;na 5</option>
-    </select>
-</div>
+
 <div class="input-prepend">
      <span class="add-on">Hodnota v&yacute;davku:</span>
      <input class="span3" type="text">
 </div>
+
 <div class="input-prepend">
-        <span class="add-on">N&aacute;zov v&yacute;davku: </span>
-    <select class="span3">
-        <option>dom&aacute;cnos&tcaron;</option>
-    </select>
+     <span class="add-on">Zaplatil: </span>
+     <select name="osoba" class="span3">
+          @foreach($osoby as $osoba)
+               <option value="{{ $osoba->id }}" @if ($vydavky[0]->id_osoba ==  $osoba->id) selected="selected" @endif> {{ $osoba->t_meno_osoby }} {{$osoba->t_priezvisko_osoby }}</option>
+          @endforeach
+     </select>
 </div>
+
 <div><input type="button" class="btn" value="Potvrdi&tcaron;" /></div>
+
 <hr>
 <h4 class="">Zoznam výdavkov</h4>
 <form id="form1" name="form1" method="post" action="">
