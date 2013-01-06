7a91d1c31bed3550d36d4e12e8b98bb8432a379f
Scarabeon <michal.bielik@gmail.com>
2013-01-06 18:07:26 +0100

    Update application/views/spendings/periodicalspending.blade.php

--- application/views/spendings/periodicalspending.blade.php
+++ application/views/spendings/periodicalspending.blade.php
@@ -12,37 +12,36 @@
           <input class="span3" type="date">
      </div>
 
-    <div class="input-prepend" style="float:left;width:350px">
-         <span class="add-on">N&aacute;zov v&yacute;davku: </span>
-        <select class="span3">
-            <option>&nbsp;</option>
-            <option>&Scaron;abl&oacute;na 1</option>
-            <option>&Scaron;abl&oacute;na 2</option>
-            <option>&Scaron;abl&oacute;na 3</option>
-            <option>&Scaron;abl&oacute;na 4</option>
-            <option>&Scaron;abl&oacute;na 5</option>
-        </select>
-    </div>
+<div class="input-prepend" style="float:left;width:350px">
+     <span class="add-on">N&aacute;zov v&yacute;davku: </span>
+    <select class="span3">
+        <option>&nbsp;</option>
+        <option>&Scaron;abl&oacute;na 1</option>
+        <option>&Scaron;abl&oacute;na 2</option>
+        <option>&Scaron;abl&oacute;na 3</option>
+        <option>&Scaron;abl&oacute;na 4</option>
+        <option>&Scaron;abl&oacute;na 5</option>
+    </select>
+</div>
 
-    <div class="input-prepend" style="float:left;width:250px">
-         <span class="add-on">Hodnota v&yacute;davku:</span>
-        <input class="span3" type="text">
-    </div>
-    <br>
-    <div class="input-prepend">
-        <span class="add-on">Zaplatil: </span>
-        <select name="osoba" class="span3">
-            <option>&Zcaron;atkovci</option>
-            <option>otec&nbsp;&Zcaron;atko</option>
-            <option>matka&nbsp;&Zcaron;atkov&aacute;</option>
-            <option>syn&nbsp;&Zcaron;atko</option>
-            <option>dc&eacute;ra&nbsp;Z&aacute;vodn&aacute;</option>
-        </select>
-    </div>
+<div class="input-prepend" style="float:left;width:200px">
+     <span class="add-on">Hodnota v&yacute;davku:</span>
+    <input class="span3" type="text">
+</div>
+<br>
+<div class="input-prepend">
+    <span class="add-on">Zaplatil: </span>
+    <select name="osoba" class="span3">
+        <option>&Zcaron;atkovci</option>
+        <option>otec&nbsp;&Zcaron;atko</option>
+        <option>matka&nbsp;&Zcaron;atkov&aacute;</option>
+        <option>syn&nbsp;&Zcaron;atko</option>
+        <option>dc&eacute;ra&nbsp;Z&aacute;vodn&aacute;</option>
+    </select>
+</div>
 
-    <div>
-        <input type="button" class="btn" value="Potvrdi&tcaron;" />
-    </div>
+<div>
+    <input type="button" class="btn" value="Potvrdi&tcaron;" />
 </div>
 
 <hr>
