9a527364a430e5b49dc2080e0016e7a074213740
Andreyco <andreyco.badin@gmail.com>
2013-01-13 23:30:34 +0100

    Vytvorenie default akcie

--- application/controllers/incomes.php
+++ application/controllers/incomes.php
@@ -6,7 +6,7 @@ class Incomes_Controller extends Base_Controller {
 	public $do = '';
 	public $od = '';
 
-	public function get_main()
+	public function get_index()
 	{
 
 		
@@ -24,39 +24,40 @@ class Incomes_Controller extends Base_Controller {
 		$view->do = '';
 		$view->od = '';
 		return $view;
-		}
+	}
 		
-		public function action_filter()
+	public function action_filter()
+	{
+		//Auth::user()->id = 2;
+		$view = View::make('incomes.main')
+		->with('active', 'prijmy')->with('subactive', 'admin/settings');
+		$view->osoby = DB::table('D_OSOBA')->where('id_domacnost', '=',Auth::user()->id)->get();
+		foreach ($view->osoby as $osoba)
 		{
-			//Auth::user()->id = 2;
-			$view = View::make('incomes.main')
-			->with('active', 'prijmy')->with('subactive', 'admin/settings');
-			$view->osoby = DB::table('D_OSOBA')->where('id_domacnost', '=',Auth::user()->id)->get();
-			foreach ($view->osoby as $osoba)
-			{
-				$id_osob[] = $osoba->id;
-			}
-			$od = Input::get('od');
-			$od = ($od!='') ? $od : '';
-		
-			$do = Input::get('do');
-			$do = ($do!='') ? $do : date('Y-m-d');
-		
-			$vydajca = Input::get('vydajca');
-			$view->partneri = DB::table('D_OBCHODNY_PARTNER')->where_in('id_osoba', $id_osob)->get();
-			$view->kategorie = Kategoria::where('id', 'LIKE','%K%')->where('id_domacnost','=',Auth::user()->id)->get();
-			$view->prijmy = Prijem::where_in('id',$id_osob)->where('d_datum', '>=', $od)->where('d_datum', '<=', $do);
-			if ($vydajca != 'all') $view->prijmy->where("id_zdroj_prijmu",'=',$vydajca);
-			$view->do = $do;
-			$view->od = $od;
-			$view->prijmy = $view->prijmy->get();
-		
-			return $view;
+			$id_osob[] = $osoba->id;
 		}
-		
+		$od = Input::get('od');
+		$od = ($od!='') ? $od : '';
+	
+		$do = Input::get('do');
+		$do = ($do!='') ? $do : date('Y-m-d');
+	
+		$vydajca = Input::get('vydajca');
+		$view->partneri = DB::table('D_OBCHODNY_PARTNER')->where_in('id_osoba', $id_osob)->get();
+		$view->kategorie = Kategoria::where('id', 'LIKE','%K%')->where('id_domacnost','=',Auth::user()->id)->get();
+		$view->prijmy = Prijem::where_in('id',$id_osob)->where('d_datum', '>=', $od)->where('d_datum', '<=', $do);
+		if ($vydajca != 'all') $view->prijmy->where("id_zdroj_prijmu",'=',$vydajca);
+		$view->do = $do;
+		$view->od = $od;
+		$view->prijmy = $view->prijmy->get();
+	
+		return $view;
 	}
 
-	
+	/**
+	 * Pridanie/editacia prijmu
+	 * @author Andreyco
+	 */
 	public function get_form()
 	{
 		$viewData = array(
@@ -65,6 +66,10 @@ class Incomes_Controller extends Base_Controller {
 		return View::make('incomes.form', $viewData);
 	}
 
+	/**
+	 * Ulozenie zmien v prijme
+	 * @author Andreyco
+	 */
 	public function post_form()
 	{
 		$rules = array(
@@ -86,8 +91,11 @@ class Incomes_Controller extends Base_Controller {
 
 	/**
 	 * AJAX actions
+	 * @author Andreyco
 	 */
 	public function post_get_source()
 	{
 		return Response::json(Prijem::get_source_list(Input::get('id')));
 	}
+
+}
