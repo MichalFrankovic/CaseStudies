8a93c8a977632c54456d89bcdb233c9b07dbfedd
Andreyco <andreyco.badin@gmail.com>
2013-01-14 14:04:04 +0100

    Controller, cely

--- application/controllers/incomes.php
+++ application/controllers/incomes.php
@@ -61,8 +61,11 @@ class Incomes_Controller extends Base_Controller {
 	public function get_form()
 	{
 		$viewData = array(
-			'list_person'	=> Prijem::get_person_for_list()
+			'list_person'	=> Prijem::get_person_for_list(),
+			'partners'		=> Prijem::get_partners(),
+			'incomes'		=> Prijem::get_incomes(),
 		);
+		// print_r($viewData['incomes']);
 		return View::make('incomes.form', $viewData);
 	}
 
@@ -72,30 +75,115 @@ class Incomes_Controller extends Base_Controller {
 	 */
 	public function post_form()
 	{
-		$rules = array(
-		    'id_osoba'	=> 'required',
-		    'd_datum'	=> 'required',
-		    'vl_suma_prijmu'	=> 'required',
-		    'id_zdroj_prijmu'	=> 'required'
+		$data = array(
+			'id_zdroj_prijmu'	=> Input::get('id_zdroj_prijmu'),
+			'vl_suma_prijmu'	=> Input::get('vl_suma_prijmu'),
+			'd_datum'			=> date('Y-m-d', strtotime(Input::get('d_datum'))),
+			't_poznamka'		=> Input::get('t_poznamka'),
 		);
-
-		$validation = Validator::make(Input::all(), $rules);
-		if($validation->fails()):
-			print_r($validation->errors);
-		endif;
-		// if ($validation->fails())
-		// {
-		//     return $validation->errors;
-		// }
+		
+		$id = DB::table('F_PRIJEM')->insert_get_id($data);
+		if($id)
+		{
+			return Redirect::to('incomes/form')
+				->with('status', 'Nový Príjem bol úspešne uložený')
+				->with('status_class', 'success');
+		} else {
+			return Redirect::to('incomes/form')
+				->with('status', 'Pri ukladaní došlo k chybe')
+				->with('status_class', 'error');
+		}
 	}
+
+	public function get_partners()
+	{
+		$viewData = array(
+			'partners'	=> Prijem::get_partners(Auth::user()->id),
+		);
+		return View::make('incomes.partners', $viewData);
+	}
+
+
+
+	/**
+	 * Vyhladaj vsetky zdroje prijmov
+	 * @author Andreyco
+	 */
+	public  function get_sources()
+	{
+		$viewData = array(
+			'sources'	=> Prijem::get_sources(),
+		);
+		// print_r($viewData['sources']);
+		return View::make('incomes.sources', $viewData);
+	}
+
+
+	public function get_delete($id)
+	{
+		if(DB::table('F_PRIJEM')->where('id', '=', $id)->delete())
+		{
+			return Redirect::to('incomes/form')
+				->with('status', 'Príjem bol odstránený')
+				->with('status_class', 'success');
+		} else {
+			return Redirect::to('incomes/form')
+				->with('status', 'Pri vykonávaní operácie došlo k chybe')
+				->with('status_class', 'error');
+		}
+	}
+
+
 
 	/**
 	 * AJAX actions
 	 * @author Andreyco
 	 */
-	public function post_get_source()
+	public function get_ajaxload($param, $id = false)
 	{
-		return Response::json(Prijem::get_source_list(Input::get('id')));
+		switch($param)
+		{
+			case 'partners':
+				$partners = array();
+				foreach(Prijem::get_partners() as $partner)
+				{
+					array_push($partners, array($partner->id => $partner->t_nazov) );
+				}
+				return Response::json($partners);
+				break;
+
+			case 'familymembers':
+				$members = array();
+				foreach(Prijem::get_person() as $member)
+				{
+					$members[$member->id] = $member->t_meno_osoby.' '.$member->t_priezvisko_osoby;
+				}
+				return Response::json($members);
+				break;
+
+			case 'incomesources':
+				return Response::json(Prijem::get_source_list( $id ));
+				break;
+		}
+	}
+
+	public function post_ajaxsave($table)
+	{
+		$data = array(
+			Input::get('name')	=> Input::get('value'),
+			'id'	=> Input::get('pk'),
+		);
+		// Transform date into valid format
+		if(Input::get('name') == 'd_datum')
+		{
+			$data['d_datum'] = date('Y-m-d', strtotime($data['d_datum']));
+		}
+		return Response::json(Prijem::ajaxsave($table, $data));
+	}
+
+	public function post_inline_edit()
+	{
+		return Response::json(Prijem::inline_save());
 	}
 
 }
