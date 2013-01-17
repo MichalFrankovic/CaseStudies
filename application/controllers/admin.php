ce6345de81e3d94e5abceefcba13935d9e948619
Dawe327 <david.kocman@gmail.com>
2013-01-17 17:04:36 +0100

    Update application/controllers/admin.php

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -32,7 +32,7 @@ class Admin_Controller extends Base_Controller {
         
         //--- EDITOVANIE UZIVATELA --- 
         public function action_editUser(){
-            $view = View::make('admin.eUser')->with('active', 'admin')->with('subactive', 'admin/users');
+            $view = View::make('admin.euser')->with('active', 'admin')->with('subactive', 'admin/users');
             $id = Input::get('id');
             $view->domacnosti = DB::table('D_DOMACNOST')->where('id', '=', $id)->get();
             
