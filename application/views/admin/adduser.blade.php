5c432488b79a34061ea053c17aa789050dabca69
David Kocman <david.kocman@gmail.com>
2013-01-21 18:16:22 +0100

    Nieco malo pre testerov (ADMIN)

---
+++ application/views/admin/adduser.blade.php
@@ -0,0 +1,90 @@
+@include('head')
+@include('admin/submenu')
+
+{{ Form::open(null, 'POST', array('class' => 'side-by-side')); }}
+
+@if (isset($error) && $error == true)
+	<div class="alert alert-error">{{ $error }}</div>
+@endif
+
+<div {{ isset($errors->name) || (is_array($errors) && isset($errors['name'])) ? ' class="control-group error"' : '' }}>
+	{{ Form::label('name', 'Názov domácnosti', array('class' => 'control-label')) }}
+	{{ Form::text('name', $name) }}
+	{{ isset($errors->name) || (is_array($errors) && isset($errors['name'])) ? '<span class="help-inline">'.$errors['name'].'</span>' : '' }}
+</div>
+
+<div {{ isset($errors->email) || (is_array($errors) && isset($errors['email'])) ? ' class="control-group error"' : '' }}>
+	{{ Form::label('email', 'E-mailová adresa', array('class' => 'control-label')) }}
+	{{ Form::text('email', $email) }}
+	{{ isset($errors->email) || (is_array($errors) && isset($errors['email'])) ? '<span class="help-inline">'.$errors['email'].'</span>' : '' }}
+</div>
+
+<div {{ isset($errors->password) || (is_array($errors) && isset($errors['password'])) ? ' class="control-group error"' : '' }}>
+	{{ Form::label('password', 'Heslo', array('class' => 'control-label')) }}
+	{{ Form::password('password') }}
+	{{ isset($errors->password) || (is_array($errors) && isset($errors['password'])) ? '<span class="help-inline">'.$errors['password'].'</span>' : '' }}
+</div>
+
+<div {{ isset($errors->password_repeat) || (is_array($errors) && isset($errors['password_repeat'])) ? ' class="control-group error"' : '' }}>
+	{{ Form::label('password_repeat', 'Potvrď heslo', array('class' => 'control-label')) }}
+	{{ Form::password('password_repeat') }}
+	{{ isset($errors->password_repeat) || (is_array($errors) && isset($errors['password_repeat'])) ? '<span class="help-inline">'.$errors['password_repeat'].'</span>' : '' }}
+</div>
+
+<div {{ isset($errors->status) || (is_array($errors) && isset($errors['status'])) ? ' class="control-group error"' : '' }}>
+	{{ Form::label('status', 'Admin práva', array('class' => 'control-label')) }}
+	{{ Form::text('status') }}
+	{{ isset($errors->status) || (is_array($errors) && isset($errors['status'])) ? '<span class="help-inline">'.$errors['status'].'</span>' : '' }}
+</div>
+
+<div class="submit">
+	{{ Form::submit('Pridať') }}
+	{{ Form::reset('Návrat', array('onclick' => 'document.location.href=\'' . URL::home() . '\';')); }}
+</div>
+
+{{ Form::close() }}
+
+
+<!--<h2>Pridanie užívateľa (domácnosti)</h2>
+<form id="form1" name="form1" method="post" action="addUserDone">
+    <table class="table table-bordered table-striped">
+        <thead>
+        <tr>
+            <th>Názov domácnosti*</th>
+            <th>E-mail*</th>
+            <th>Heslo*</th>
+            <th>Zopakovať heslo*</th>
+            <th>Úroveň účtu*</th>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>            
+            <td><div class="input-append">
+                <input name="name" class="span2" type="text" value="" placeholder="Novákovci" />
+            </div></td>
+            
+            <td><div class="input-append">
+                <input name="email" class="span2" type="text" value="" placeholder="novak@mail.com"/>
+            </div></td>
+            
+            <td><div class="input-append">
+                <input name="password" class="span2" type="password" value="" placeholder="**********"/>
+            </div></td>
+            
+            <td><div class="input-append">
+                <input name="password-repeat" class="span2" type="password" value="" placeholder="**********"/>
+            </div></td>
+            
+            <td><div class="input-append">
+                <input name="uroven" class="span2" type="text" value="" placeholder="A/N"/>
+            </div></td>
+            
+            <td><input type="submit" name="Submit" value="Pridaj používateľa"/></td>
+            
+        </tr>
+        </tbody>
+    </table>
+</form>-->
+
+@include('foot');
--- application/views/admin/index.blade.php
+++ application/views/admin/index.blade.php
@@ -1,36 +1,48 @@
 @include('head')
-
-<!--<pre>TODO:
-
-* lisitng pouzivatelov (domacnosti) - check!
-* pridanie / editacia (zmena emailu, hesla, nazvu uctu, urovne - ci je admin)
-* aktivacia / deaktivacia (fl aktivna N) - check!
-* mazanie pouzivatelov (fl aktvina na N)
-
-</pre>-->
+@include('admin/submenu')
 @if (isset($message) )
-    <h3 style="color: #bc8f8f;">{{ $message }}</h3>
+    <h2 style="color: #bc8f8f;">{{ $message }}</h2>
 @endif
 
-<!--PRIDAVANIE UZIVATELOV-->
+<!--CHECK ALL SKRIPT-->
+<script type="text/javascript">
+checked=false;
+function checkedAll (form1) {
+	var aa= document.getElementById('form1');
+	 if (checked == false)
+          {
+           checked = true
+          }
+        else
+          {
+          checked = false
+          }
+	for (var i =0; i < aa.elements.length; i++) 
+	{
+	 aa.elements[i].checked = checked;
+	}
+      }
+</script>
 
-<h2 class="">Pridanie užívateľa (domácnosti)</h2>
+<!--PRIDAVANIE UZIVATELOV-->
+<!--<a class="btn" href="admin/addUser">Pridaj používateľa</a>-->
+<!--<h2>Pridanie užívateľa (domácnosti)</h2>
 <form id="form1" name="form1" method="post" action="admin/addUser">
     <table class="table table-bordered table-striped">
         <thead>
         <tr>
-            <th>Názov domácnosti</th>
-            <th>E-mail</th>
-            <th>Heslo</th>
-            <th>Zopakovať heslo</th>
-            <th>Úroveň účtu</th>
+            <th>Názov domácnosti*</th>
+            <th>E-mail*</th>
+            <th>Heslo*</th>
+            <th>Zopakovať heslo*</th>
+            <th>Úroveň účtu*</th>
             <th></th>
         </tr>
         </thead>
         <tbody>
         <tr>            
             <td><div class="input-append">
-                <input name="domacnost" class="span2" type="text" value="" placeholder="Novákovci" />
+                <input name="name" class="span2" type="text" value="" placeholder="Novákovci" />
             </div></td>
             
             <td><div class="input-append">
@@ -38,15 +50,15 @@
             </div></td>
             
             <td><div class="input-append">
-                <input name="heslo" class="span2" type="password" value="" placeholder="**********"/>
+                <input name="password" class="span2" type="password" value="" placeholder="**********"/>
             </div></td>
             
             <td><div class="input-append">
-                <input name="heslo2" class="span2" type="password" value="" placeholder="**********"/>
+                <input name="password-repeat" class="span2" type="password" value="" placeholder="**********"/>
             </div></td>
             
             <td><div class="input-append">
-                <input name="uroven" class="span2" type="text" value="" placeholder="A/N"/>
+                <input name="status" class="span2" type="text" value="" placeholder="A/N"/>
             </div></td>
             
             <td><input type="submit" name="Submit" value="Pridaj používateľa"/></td>
@@ -54,36 +66,46 @@
         </tr>
         </tbody>
     </table>
-</form>
+</form>-->
 
 <!--LISTING A NASTAVENIE UZIVATELOV-->
-
-<h2 class="">Zoznam užívateľov (domácností)</h2>
-<form id="form1" name="form1" method="post" action="">
+<form id="form1" name="form1" method="post" action="admin/editMore">
     <table class="table table-bordered table-striped">
         <thead>
         <tr>
+            <th><input type='checkbox' name='checkall' onclick='checkedAll(form1);'></th>
             <th>Názov domácnosti</th>
             <th>E-mail</th>
-            <th>Admin účet</th>
+            <th>Admin práva</th>
             <th>Aktívna</th>            
-            <th></th>
+            <th>Nastavenia</th>
         </tr>
         </thead>
         <tbody>
         @foreach ($domacnosti as $domacnost)
         <tr>
+            <td><input type="checkbox" name="polozka[]" value="{{ $domacnost->id }}" /></td>
             <td>{{ $domacnost->t_nazov_domacnosti }}</td>
             <td>{{ $domacnost->t_email_login }}</td>
             <td>{{ $domacnost->fl_admin }}</td>
             <td>{{ $domacnost->fl_aktivna }}</td>
-            <td><a class="btn" href="admin/editUser?id={{ $domacnost->id }}">Upraviť</a>
-                <a class="btn" href="admin/disableUser?id={{ $domacnost->id }}">Aktivovať/Deaktivovať</a>
-                <a class="btn" href="admin/deleteUser?id={{ $domacnost->id }}">Zmazať</a>
+            <td><a class="btn" href="admin/editUser?id={{ $domacnost->id }}">Edit</a>
+                <a class="btn" href="admin/disableUser?id={{ $domacnost->id }}">Aktivuj/Deaktivuj</a>
+                <a class="btn" href="admin/deleteUser?id={{ $domacnost->id }}" onclick="return confirm('Určite chcete zmazať tento záznam?')">Zmaž</a>
             </td>
         </tr>
         @endforeach
+        
         </tbody>
+        <tr>
+            <td><input type="Submit" name="Submit" value="Zmaž" onclick="return confirm('Určite chcete zmazať tieto záznamy ?')"></td>
+            <td><input type="Submit" name="Submit" value="Aktivuj"> <input type="Submit" name="Submit" value="Deaktivuj"></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
     </table>
 </form>
 @include('foot');
+<!--
+-->
