5c5226ceb0b0fbfac85eeb06f1e7c7d054ca1941
AnkhaaS <tourist_mgl@yahoo.com>
2013-03-13 21:14:15 +0100

    Update sprava-typu-prijmu.blade.php
    form "upravit" pridany

--- application/views/ciselniky/sprava-typu-prijmu.blade.php
+++ application/views/ciselniky/sprava-typu-prijmu.blade.php
@@ -7,26 +7,77 @@
 @include('ciselniky/ciselniky-podmenu')
 
 
+<?php
+
+if (isset($editovany_zaznam))
+ $editacia="ano";
+    else $editacia = "nie";
+
+
+?>
 
 <div class="thumbnail" >
-    <h2>Pridajte typ príjmu</h2>
 
-{{ Form::open('ciselniky/pridajtypprijmu', 'POST', array('class' => 'side-by-side','id' => 'aktualnyformular')); }}
+<?php
+if ($editacia == 'ano') {
+     echo "<h2>    Upravte typ prijmu   </h2>";
+     echo '<form class="side-by-side" id="aktualnyformular" method="POST" action="upravtypprijmu" accept-charset="UTF-8">';  
+ }
+   else  {         
+    echo "<h2>    Pridajte typ prijmu  </h2>";
+    echo '<form class="side-by-side" id="aktualnyformular" method="POST" action="pridajtypprijmu" accept-charset="UTF-8">';
+         }
+
+?>
+
+    <input class="span4" type="hidden" name="id" value="<?php
+                                                                if (isset($editovany_zaznam[0]->id))
+                                                                    echo ($editovany_zaznam[0]->id); 
+                                                             ?>">
+    
 
     <div class="input-prepend">
-         <label class="control-label">    Názov typu príjmu:          </label>
-        <input class="span3" type="text" name="nazov_typu" value="">
+        <label class="control-label">    Názov typu:          </label>
+        <input class="span4" type="text" name="nazov_typu" value="<?php
+                                                                if (isset($editovany_zaznam[0]->t_nazov_typu))
+                                                                    echo ($editovany_zaznam[0]->t_nazov_typu); 
+                                                             ?>">
     </div>
 
-     <button onclick="formReset()" type="button" class="btn btn-primary">
-        <i class="icon-remove icon-white"></i>
-            Cancel
-    </button>
-    
-    <button type="submit" class="btn btn-primary">
-        <i class="icon-ok icon-white"></i>
-            Pridaj
-    </button>
+
+<?php
+
+if ($editacia == "ano") {
+    echo ' <a  href="sprava_typu_prijmu">
+                <button type="button" class="btn btn-primary">
+                    <i class="icon-remove icon-white"></i>
+                        Cancel
+                 </button>
+           </a>';
+
+    echo '       <button type="submit" class="btn btn-primary">
+                    <i class="icon-ok icon-white"></i>
+                        Aktualizuj
+                 </button>
+         ';
+    }
+   else {echo ' <button type="reset" class="btn btn-primary">
+                    <i class="icon-remove icon-white"></i>
+                        Cancel
+                </button>
+              ';
+
+         echo ' <button type="submit" class="btn btn-primary">
+                    <i class="icon-ok icon-white"></i>
+                        Pridaj
+                </button>
+              ';
+
+        }
+
+?>
+
+
 
 {{ Form::close() }}
 </div>
@@ -49,8 +100,9 @@
             <td><input type="checkbox" name="typ[]" id="checkbox2" class="spendcheck" value="{{ md5($typ->id). $secretword}}" /></td>
             <td> {{ $typ->t_nazov_typu }} </td>
            
-            <td> <a class="btn" href="upravittypprijmu?id={{ $typ->id }}"> Upraviť </a>
-                 <a class="btn" href="zmazattypprijmu?typ={{ md5($typ->id). $secretword}}"><i class="icon-remove"></i>Vymazať</a></td>
+            <td> <a class="btn" href="sprava_typu_prijmu?id={{ $typ->id }}"> Upraviť </a>
+                 <a class="btn" href="zmazattypprijmu?typ={{ md5($typ->id). $secretword}}" onclick="return confirm('Určite chcete zmazať tento záznam?')">
+                    <i class="icon-remove"></i>Vymazať</a></td>
         </tr>
         @endforeach
     </tbody>
