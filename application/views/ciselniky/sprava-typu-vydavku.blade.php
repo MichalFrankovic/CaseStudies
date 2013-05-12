6c25769c13be434bfe386ccc0e8936fffba4c9f0
MichalFrankovic <frankovic.michal@gmail.com>
2013-05-12 18:42:48 +0200

    ...

--- application/views/ciselniky/sprava-typu-vydavku.blade.php
+++ application/views/ciselniky/sprava-typu-vydavku.blade.php
@@ -44,7 +44,7 @@ if ($editacia == 'ano') {
        <span style="padding:0px 10px;"> <input class="span3" type="text" name="nazov_typu_vydavku" value="<?php
                                                                 if (isset($meneny_vydavok))
                                                                  echo $meneny_vydavok; 
-                                                                if (isset($editovany_zaznam[0]->t_nazov_typu_vydavku))
+                                                                elseif (isset($editovany_zaznam[0]->t_nazov_typu_vydavku))
                                                                     echo ($editovany_zaznam[0]->t_nazov_typu_vydavku); 
                                                              ?>">
 </span>{{ isset($errors->typvydavku) || (is_array($errors) && isset($errors['typvydavku'])) ? '<span class="help-inline">'.$errors['typvydavku'].'</span>' : '' }}
