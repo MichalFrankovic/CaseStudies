ecf22b5234b9d153e1315147a095fde30e499bc6
MichalFrankovic <frankovic.michal@gmail.com>
2013-05-16 11:54:00 +0200

    Sloník :)

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -7,9 +7,44 @@
 	</div>
 @endif
 
-<h2>Výdavkovač - čo je to?</h2>
 
-<p>Príjmovo výdavkový systém pre domácnosti</p>
+<h2 style="color:white;"> 	Výdavkovač - čo je to? </h2>
+
+<p style="color:white;">	Príjmovo výdavkový systém pre domácnosti </p>
 
 
-@include('foot')
+<style type="text/css">
+	body { 
+	  background: url({{URL::to('../application/obrazky/obr.jpg')}}) no-repeat fixed 100% 100%; 
+	  -webkit-background-size: cover;
+	  -moz-background-size: cover;
+	  -o-background-size: cover;
+	  background-size: cover;
+	  position:absolute; width:100%;
+	}
+
+	.navbar-inner {background: transparent !important;}
+
+	.logo{opacity: 0;}
+
+	.wrapper{background: transparent !important;}
+
+	small{color: white !important;}
+
+	a{color: white;}
+	a:hover{color: white;}
+
+</style>
+
+{{ HTML::script('assets/js/jquery.js') }}
+
+<script type="text/javascript">
+
+$(document).ready(function(){
+
+	$('#head').slideDown('fast');
+	$('#content').slideDown('slow');
+
+});
+
+</script>
